7c61931810c7c6fde40de7b1c8429a20f60ef437
rlagmlfk <gmlfk703@gmail.com>
2022-12-05 21:17:36 +0900

    중복등록과 충돌

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -13,5 +16,15 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    // 이 경우 수동 빈 등록이 우선권을 가진다
+    // 수동 빈이 자동 빈을 오버라이딩 해버림
+    // 하지만 버그가 발생할 수 있기에
+    // 최근의 스프링에서는 에러메시지를 띄움(부트로 실행시)
+    // application.properties에서 설정 가능
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 
 }
